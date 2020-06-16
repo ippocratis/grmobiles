4be07980aa759e3add4cb754f0e67478b6aadb33
ippocratis <52239579+ippocratis@users.noreply.github.com>
2020-06-16 12:38:12 +0300

    comenting

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 // Utils
-const Telegraf = require('telegraf');
+const Telegraf = require('telegraf'); //npm install telegraf
 
 const Telegram = require('telegraf/telegram')
 
-const telegram = new Telegram('xxx');
+const telegram = new Telegram('xxx'); //bot token
 
-const bot = new Telegraf('xxx');
+const bot = new Telegraf('xxx'); //bot token
 
-const ms = require("ms");
+const ms = require("ms");  //npm install ms
 
 const Extra = require('telegraf/extra')
 
@@ -38,7 +38,7 @@ insult(bot);
 
 
 //unrestrict group id
-const groupId = -xxx
+const groupId = -xxx  //group id
 
 //restrict options
 const options = {
@@ -65,43 +65,40 @@ const options = {
 //////////////////////////////////////////////
 
 //start message on pm
-//bot.start((ctx) => ctx.reply("γράψε ολογράφως και με κεφαλαία πόσο κάνει έντεκα και τέσσερα;   και  αν αντιμετοπίζεις καποιο πρόβλημα επικοινώνησε με τον @ippokratis"));
+
 
 bot.start((ctx, next) => {
   if( ctx.chat.type !== 'private' ) return
-  ctx.reply("γράψε ολογράφως , σε μια λέξη , χωρίς τόνους και με κεφαλαία πόσο κάνει έντεκα και τέσσερα.Αν αντιμετωπίζεις καποιο πρόβλημα επικοινώνησε με τον @ippokratis")
+  ctx.reply("xx") //right your question here
   next(ctx);
 })
 
 
 //reply on correct answer
 
-bot.hears(["ΔΕΚΑΠΕΝΤΕ","ΔΕΚΑΠΈΝΤΕ","ΔΕΚΑ ΠΕΝΤΕ","ΔΈΚΑ ΠΈΝΤΕ"], (ctx, next) => {
-  //if( ctx.chat.type === 'private' ){
-   // ctx.reply('μπορεις να γραψεις στο γκρουπ @hellasmobiles τωρα')
-//ctx.reply('τσεκ',
-    // )}
-  
+bot.hears(["first answer","second answer"], (ctx, next) => { //bot will reply upon hearing those words
+    
   if( ctx.chat.type !== 'private' ) return
-  let beginMessage = "μπορεις να γραψεις στο γκρουπ @hellasmobiles τωρα"
-  bot.telegram.sendMessage(ctx.chat.id, beginMessage,
+  let beginMessage = "xxx" //reply on correct answer
+  //comment or delet from here if you dont want buttons on your reply
+  bot.telegram.sendMessage(ctx.chat.id, beginMessage, //buttons reply on correct answer
 {
   reply_markup: {
     inline_keyboard: [
       [
-        { text: "κανονες", url: 'https://telegra.ph/Rules-01-17-3' },
-        { text: "οδηγίες", url: 'https://telegra.ph/Directions-03-07' },
-        { text: "σημειώσεις", callback_data: 'notera'}
+        { text: "xxx", url: 'https://yoururl' } //you can have your customise ypur button keyborad as in notes.js
+        
       ]
     ]
   }
 })
+	//comment or delet untill here if you dont want buttons on your reply
 next(ctx);
 })
 
 
 //unrestrict on correct unswer
-bot.hears(["ΔΕΚΑΠΕΝΤΕ","ΔΕΚΑΠΈΝΤΕ","ΔΕΚΑ ΠΕΝΤΕ","ΔΈΚΑ ΠΈΝΤΕ"], (ctx, next) => {
+bot.hears(["first answer","second answer"], (ctx, next) => { //bot unmute users in group upon hering those words (the corect ansers)
   ctx.tg.restrictChatMember(groupId, ctx.from.id, unoptions)
 next(ctx);  
 })
@@ -109,7 +106,7 @@ next(ctx);
 
 
 //restrict on wellcome on groups
-bot.on('new_chat_members', (ctx, next) => {
+bot.on('new_chat_members', (ctx, next) => { //bot mutes users when they join group
   ctx.restrictChatMember(ctx.from.id, options)
   next(ctx);
   });
@@ -117,27 +114,28 @@ bot.on('new_chat_members', (ctx, next) => {
 //welcome message on groups
 bot.on('new_chat_members', (ctx, next) => {
   ctx.replyWithMarkdown(` [${ctx.message.from.first_name}](tg://user?id=${
-            ctx.message.from.id
+            ctx.message.from.id 
         })` + ' καλως ηρθες. ', { reply_to_message_id: ctx.message.message_id })
         .then(message => {
           setTimeout(function (){
               ctx.telegram.deleteMessage(ctx.chat.id, message.message_id);
-          },  10 * 60 * 1000)
+          },  10 * 60 * 1000) //bot deletes welcome message after a timeout (in miliseconds)
 
 
-       ctx.replyWithMarkdown('Πάτα στο κουμπι start test και απάντησε στην ερώτηση που θα σου γίνει εκει για να μπορείς να μιλήσεις',
+       ctx.replyWithMarkdown('xxx', //the welcome text (you could give some instructions here)
           
           {
 
             reply_markup: {
               inline_keyboard: [
+		      //if you just want the start test button delete from here
                 [
-                  { text: "κανονες", url: 'https://telegra.ph/Rules-01-17-3' },
-                  { text: "οδηγίες", url: 'https://telegra.ph/Directions-03-07' }
-                  //{ text: "σημειώσεις", callback_data: 'notera'}           
+                  { text: "rules", url: 'https://telegra.ph/xxx' }    //you could create a telegraph doc with your rules and add the link here         
                 ],
+		      //if you just want the start test button delete until here
+
                 [
-                  { text: "start test", url: 'https://t.me/hellasmobiles_admin_bot?start=test' }
+                  { text: "start test", url: 'https://t.me/yourbotname?start=test' } //deep linking , replace yourbotname with your bots name so that when users hit this button they will redirected to bot and a start button will wait them there to start the test
                 ] 
               ]
             }
@@ -147,61 +145,51 @@ bot.on('new_chat_members', (ctx, next) => {
         .then(message => {
           setTimeout(function (){
               ctx.telegram.deleteMessage(ctx.chat.id, message.message_id);
-          },  10 * 60 * 1000)         
+          },  10 * 60 * 1000)   //bot deletes the buttons after a timeout (in miliseconds)        
 
       })
   })
   next(ctx);
       });
   
-  //welcome σημειωσεις sub menu
-  bot.action('notera', ctx => {
-    let noteraMessage = `
-Για προβολή των σημειώσεων του γρουπ:
-Γραψε /notes για εμφάνιση τους σε hashtag.
-Γράψε /memo για προβολή τους σε μορφή μενου => υπομενού .
-Μπορείς να χρησιμοποιήσεις το /memo και σε προσωπικό μήνυμα με το μποτ διαχείρισης GR mobiles ανενόχλητος.`;
-    ctx.deleteMessage();
-    bot.telegram.sendMessage(ctx.chat.id, noteraMessage,      
-      )
-      .then(message => {
-        setTimeout(function (){
-            ctx.telegram.deleteMessage(ctx.chat.id, message.message_id);
-        },  10 * 1000)     
-    })
-  })
-  
+ 
 
 //autoban
+//auto kick timeout starts here delete if you want to ban
+function delay(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
+}
+//auto kick timeout ends here delete if you want to ban
+
 var t;
 bot.on('new_chat_members', (ctx, next) => {
   t = setTimeout(function() {
     ctx.kickChatMember(ctx.message.from.id);
+    delay(120000).then(() => telegram.unbanChatMember(ctx.chat.id, ctx.message.from.id)); //bot will unban so he will only be kicked and can rejoin if he wants . delete this line if you dont want him to be able to rejoin
     telegram.unbanChatMember(ctx.chat.id, ctx.message.from.id); 
     ctx.replyWithMarkdown('o ' + ` [${ctx.message.from.first_name}](tg://user?id=${
       ctx.message.from.id
-  })` + ' δεν απαντησε σωστα και τον kikάρω');
-   }, 10 * 60 * 1000);
+  })` + ' δεν απαντησε σωστα και τον kikάρω');  //bot sends this message in group after banned user
+   }, 10 * 60 * 1000); //after that timeout bot will ban user 
    next(ctx);
 });
 bot.on("text", (ctx, next) => {
-  if(ctx.update.message.text ===  ('ΔΕΚΑΠΕΝΤΕ') && ctx.chat.type === 'private' ) {
+  if(ctx.update.message.text ===  ('first answer') && ctx.chat.type === 'private' ) { //upon hearing the correct bot will stop the timeout and user will not kicked or banned 
     clearTimeout(t);
      } 
      next(ctx);
 });
 
-//pin
-bot.command("pinit", (ctx, next) => {
-  ctx.pinChatMessage(ctx.chat.id,ctx.message.reply_to_message.id);
-  next(ctx);
-  })
+
 
 
 //forwarder
+//if you want to know the answers user gives to the bot.bot will forward all messages to you
 bot.on('message', (ctx, next) => {
 	if( ctx.chat.type === 'private' ){
-		ctx.forwardMessage(531648989)}
+		ctx.forwardMessage(xxx)} //your user id
 		next(ctx);
 })
 

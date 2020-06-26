2cddc015b6c4799a8e1fe3de6624823102df2e99
ippocratis <52239579+ippocratis@users.noreply.github.com>
2020-06-26 15:38:53 +0300

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,11 +1,14 @@
 module.exports = (bot) => {
-  const axios = require('axios');
+  
+  //stats require external api . i used https://grstats.me/docs wich require their bot to be pressent in the group to gather the data.
+  
+  const axios = require('axios');  //npm install axios
 
     bot.command('stattop24h', (ctx) => {
-        axios.get('https://grstats.me/api/chat/wguffp7xi3ffxtywnf53?timezone=3')
+        axios.get('https://grstats.me/api/chat/bot_token') //your bot token
           .then(res => {     
-           var ddddd = res.data.res.top_users_24h;
-           ctx.reply('οι πιο δραστήριοι χρήστες το τελευταίο 24ωρο ' + "\n" + "\n" +
+           var ddddd = res.data.res.top_users_24h;  //7 most last 24 hours active users
+           ctx.reply('xxx ' + "\n" + "\n" +
              (ddddd[0][0] + ` ${ ddddd[1][0]}`) + "\n"
            + (ddddd[0][1] + ` ${ ddddd[1][1]}`) + "\n"
            + (ddddd[0][2] + ` ${ ddddd[1][2]}`) + "\n"
@@ -21,10 +24,10 @@ module.exports = (bot) => {
       ///////////////////////////////////////////////////
       ///////////////////////////////////////////////
       bot.command('stattop', (ctx) => {
-        axios.get('https://grstats.me/api/chat/wguffp7xi3ffxtywnf53?timezone=3')
+        axios.get('https://grstats.me/api/chat/bot_token') //bot token
           .then(res => {
-           var ddddd = res.data.res.top_users;
-           ctx.reply('οι πιο δραστήριοι χρήστες τον τελευταίο μήνα ' + "\n" + "\n" +
+           var ddddd = res.data.res.top_users; //most active users the last month
+           ctx.reply('xxxx ' + "\n" + "\n" +
            (ddddd[0][1] + ` ${ ddddd[0][2]}`) + "\n" +
            (ddddd[1][1] + ` ${ ddddd[1][2]}`) + "\n" +
            (ddddd[2][1] + ` ${ ddddd[2][2]}`) + "\n" +
@@ -39,17 +42,17 @@ module.exports = (bot) => {
 
       //////////////////////////////////////////
       bot.command('statavrg', (ctx) => {
-        axios.get('https://grstats.me/api/chat/wguffp7xi3ffxtywnf53?timezone=3')
+        axios.get('https://grstats.me/api/chat/bot_token') //bot token
           .then(res => {
             const average = list => list.reduce((prev, curr) => prev + curr) / list.length;
-            const list = res.data.res.daily_users_messages[1][1]
+            const list = res.data.res.daily_users_messages[1][1]   //all time average daily messages
              average(list)
              const average1 = list1 => list1.reduce((prev, curr) => prev + curr) / list1.length;
-             const list1 = res.data.res.daily_users_messages[1][0]
+             const list1 = res.data.res.daily_users_messages[1][0]  //all time average daily active users 
               average1(list1)
       
-            ctx.reply('ημερήσιος μέσος όρος μηνυμάτων στο γκρούπ ' + "\n" + average(list) + "\n" + "\n" +
-            'ημερήσιος μέσος όρος χρηστών στο γκρούπ '+ "\n" + average1(list1)
+            ctx.reply('xxx ' + "\n" + average(list) + "\n" + "\n" +
+            'xxx '+ "\n" + average1(list1)
             )
           }).catch(e => {
             console.log(e);
